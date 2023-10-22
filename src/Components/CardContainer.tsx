b113e1b9d7d3e77454442959a58fb2f4dae80c67
Mike Odnis <airwolf635@gmail.com>
2023-10-21 23:24:36 -0400

    Quick commit

--- src/Components/CardContainer.tsx
+++ src/Components/CardContainer.tsx
@@ -1,10 +1,11 @@
+import React from 'react'
 const CardContainer = (props: Data) => {
-  const { id, name, description, link, sourceSVG, type } = props
+  const { id, name, description, link, sourceSVG: SVGComponent, type } = props
   return (
     <>
       <article id={`Card ${id}`} className='rounded-[6px] shadow-md flex flex-col relative box-content w-[90%] h-[200px] bg-blue-400'>
         <a href={`${link}`} target={`_blank`} rel={`noopener noreferrer`} className='h-[100%] flex flex-col'>
-          {sourceSVG && ( <sourceSVG /> )}
+          {SVGComponent && React.createElement(SVGComponent as React.ComponentType<React.SVGProps<SVGSVGElement>>)}
           <div className='flex flex-col py-[12px] px-[16px] grow card-body bg-yellow-400'>
               <h2 className='text-[17px] leading-[24px] font-[600] line-clamp-3 card-title'>
                 {name}
